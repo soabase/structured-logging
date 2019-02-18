3522a1c784c59216692b75499ee3834e2a1b11f5
randgalt <jordan@jordanzimmerman.com>
2019-02-18 12:22:28 -0500

    test predefined and exceptions

--- src/test/java/io/soabase/structured/logger/Temp.java
+++ src/test/java/io/soabase/structured/logger/Temp.java
@@ -6,11 +6,19 @@ import org.slf4j.LoggerFactory;
 
 public class Temp {
     @Test
-    public void temp()
+    public void testBasic()
     {
         Logger logger = LoggerFactory.getLogger(getClass());
         // TODO make the StructuredLogger injectable, etc.
         StructuredLogger<TestSchema> log = StructuredLoggerFactory.get(logger, TestSchema.class);
         log.debug(s -> s.context("one").event("two").id("three").catchAll("hey", "there"));
+    }
+
+    @Test
+    public void testPredefined() {
+        PredefinedStructuredLoggerFactory.predefine(LoggerFactory.getLogger(getClass()), TestSchema.class);
+
+        StructuredLogger<TestSchema> log = PredefinedStructuredLoggerFactory.get(getClass());
+        log.debug("Hey", new Error("what"), s -> s.context("one").event("two").id("three").catchAll("hey", "there"));
     }
 }
