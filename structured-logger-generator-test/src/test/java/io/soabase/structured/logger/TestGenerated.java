279d06a5fbf592e9cf086dcb485b734efac2531b
Jordan Zimmerman <jordan@jordanzimmerman.com>
2019-02-18 20:14:20 -0500

    Update TestGenerated.java

--- structured-logger-generator-test/src/test/java/io/soabase/structured/logger/TestGenerated.java
+++ structured-logger-generator-test/src/test/java/io/soabase/structured/logger/TestGenerated.java
@@ -6,7 +6,7 @@ import io.soabase.structured.logger.schemas.Qty;
 import io.soabase.structured.logger.slf4j.StructuredLoggerFactory;
 import org.junit.Test;
 
-@LoggerSchema({Id.class, Qty.class})
+@LoggerSchema({Id.class, Qty.class}) // <--- Preprocessor generates a "schema" class named TestGeneratedSchema
 public class TestGenerated {
     @Test
     public void testGenerated() {
